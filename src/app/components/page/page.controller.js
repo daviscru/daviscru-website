ac856d5977a154ae70c40896e9a43f65ef06029a
Scotty Waggoner <ozzieorca@gmail.com>
2015-09-18 00:14:34 -0400

    Make widget buttons (alignment, divider, reorder, delete) work.

--- src/app/components/page/page.controller.js
+++ src/app/components/page/page.controller.js
@@ -10,13 +10,54 @@
     var vm = this;
     vm.auth = authentication;
     vm.loadStatus = 'loading';
-    var fbWidgetsObj = fbWidgets(currentPage.url);
-    fbWidgetsObj.$bindTo($scope, 'widgets').then(function(){
-      if($scope.widgets[0] === undefined){
-        vm.loadStatus = 'error';
-      }else{
-        vm.loadStatus = 'success';
+    vm.swapAlignment = swapAlignment;
+    vm.toggleDivider = toggleDivider;
+    vm.moveWidgetUp = moveWidgetUp;
+    vm.moveWidgetDown = moveWidgetDown;
+    vm.deleteWidget = deleteWidget;
+
+    activate();
+
+    function activate() {
+      var fbWidgetsObj = fbWidgets(currentPage.url);
+      fbWidgetsObj.$bindTo($scope, 'widgets').then(function () {
+        if ($scope.widgets[0] === undefined) {
+          vm.loadStatus = 'error';
+        } else {
+          vm.loadStatus = 'success';
+        }
+      });
+    }
+
+    function swapAlignment(widget){
+      widget.alignment == "left" ? widget.alignment = "right" : widget.alignment = "left";
+    }
+    function toggleDivider(widget){
+      widget.hasDivider = !widget.hasDivider;
+    }
+
+    function moveWidgetUp(index){
+      if($scope.widgets[index - 1] !== undefined){
+        var currentWidget = $scope.widgets[index];
+        $scope.widgets[index] = $scope.widgets[index - 1];
+        $scope.widgets[index - 1] = currentWidget;
       }
-    });
+    }
+
+    function moveWidgetDown(index){
+      if($scope.widgets[index + 1] !== undefined){
+        var currentWidget = $scope.widgets[index];
+        $scope.widgets[index] = $scope.widgets[index + 1];
+        $scope.widgets[index + 1] = currentWidget;
+      }
+    }
+
+    function deleteWidget(index){
+      for(var i = index; $scope.widgets[i + 1] != undefined; i++){
+        $scope.widgets[i] = $scope.widgets[i + 1];
+        delete $scope.widgets[i + 1];
+      }
+    }
+
   }
 })();
