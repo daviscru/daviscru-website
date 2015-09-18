b1c0c64ea24c6ab846a37c1a438d4280a03d7cca
Scotty Waggoner <ozzieorca@gmail.com>
2015-09-18 01:24:11 -0400

    Update default widget data. Allow adding widgets to error page to create new page. Make all addWidget buttons add widgets with default values. Hide unused buttons.

--- src/app/components/page/page.controller.js
+++ src/app/components/page/page.controller.js
@@ -33,8 +33,30 @@
 
     function addWidget(type){
       var i;
-      for(i = 0; $scope.widgets[i + 1] !== undefined; i++){};
-      $scope.widgets[i + 1] = {widgetType: type};
+      for(i = 0; $scope.widgets[i] !== undefined; i++){};
+      $scope.widgets[i] = {
+        widgetType: type,
+        alignment: 'left',
+        panelContent: {
+          0: {
+            headingText: 'Panel 1',
+            image: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=360%C3%97360&w=360&h=360'
+          },
+          1: {
+            headingText: 'Panel 2',
+            image: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=360%C3%97360&w=360&h=360'
+          },
+          2: {
+            headingText: 'Panel 3',
+            image: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=360%C3%97360&w=360&h=360'
+          }
+        },
+        images: {
+          0: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=500%C3%97500&w=500&h=500',
+          1: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=500%C3%97500&w=500&h=500',
+          2: 'https://placeholdit.imgix.net/~text?txtsize=47&txt=500%C3%97500&w=500&h=500'
+        }
+      };
     }
 
     function swapAlignment(widget){
